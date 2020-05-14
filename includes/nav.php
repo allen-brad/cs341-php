bfa062c31ce2fac6158efcbdd8d85841a852783e
Brad R. Allen <bradrallen@gmail.com>
2020-05-13 19:49:47 -0600

    working

--- includes/nav.php
+++ includes/nav.php
@@ -9,7 +9,15 @@
         <a class="nav-link" href="about.php">About Me<?php if($_SERVER['SCRIPT_NAME']=="/about.php") echo '<span class="sr-only">(current)</span>'; ?></a>
       </li>
       <li class="nav-item <?php if($_SERVER['SCRIPT_NAME']=="/assignments.php") echo 'active'; ?>">
-        <a class="nav-link" href="assignments.php">Assignments<?php if($_SERVER['SCRIPT_NAME']=="/about.php") echo '<span class="sr-only">(current)</span>'; ?></a>
+        <a class="nav-link" href="assignments.php">Weekly<?php if($_SERVER['SCRIPT_NAME']=="/about.php") echo '<span class="sr-only">(current)</span>'; ?></a>
+      </li>
+      <li class="nav-item dropdown <?php if($_SERVER['SCRIPT_NAME']=="/assignments.php") echo 'active'; ?>">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Week 01</a>
+              <a class="dropdown-item" href="#">Week 02</a>
+              <a class="dropdown-item" href="/assignmentW03.php">Week 03</a>
+            </div>
       </li>
     </ul>
   </div>
