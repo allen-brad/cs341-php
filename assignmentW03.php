477f41edde6ec60125d234153badbf5b533e9c09
Brad R. Allen <bradrallen@gmail.com>
2020-05-14 16:07:36 -0600

    nav bar btn

--- assignmentW03.php
+++ assignmentW03.php
@@ -155,7 +155,7 @@ switch ($action) {
         echo '<li>'.$fruits[$key]['desc']. '</li>';
         echo '</ul>';
         echo '<div class="container"><div class="row mt-2">  <div class="form-group">';
-        echo '<label class="d-inline-block" for="quantity">Quantity: </label>';
+        echo '<label class="d-inline-block" for="quantity">Quantity:&nbsp</label>';
         echo '<select name="quantity" class="form-control form-control-sm d-inline-block" style="width: auto;" id="quantity">';
         echo '<option value="1">1</option>   <option value="2">2</option>   <option value="3">3</option>   <option value="4">4</option>   <option value="5">5</option>';
         echo '</select>   </div>   </div>   </div>';
