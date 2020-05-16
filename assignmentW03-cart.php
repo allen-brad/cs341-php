45523ebccfbb9728545453b48bf27e82c20ad36a
Brad R. Allen <bradrallen@gmail.com>
2020-05-16 11:52:32 -0600

    cart

--- assignmentW03-cart.php
+++ assignmentW03-cart.php
@@ -145,48 +145,19 @@ switch ($action) {
                         $total = 0;
                         foreach($_SESSION['cart'] as $item){
                             $total += $fruits[$item['product']]['price']*$item['quantity'];
-                            echo '<li class="list-group-item d-flex justify-content-between lh-condensed">;';
+                            echo '<li class="list-group-item d-flex justify-content-between lh-condensed">';
                             echo '<di>';
                             echo '<h6 class="my-0">'. $item['product'] . '</h6>';
-                            echo '<small class="text-muted">'. $fruits[$item['product']]['desc'] . '</small>';
-                            echo '<small class="text-muted">'. $item['quantity'] . '</small>';
+                            echo '<small class="text-muted">'. $fruits[$item['product']]['desc'] . '<br>Quantity:'.$item['quantity']. '</small>';
                             echo '</di>';
                             echo '<span class="text-muted">$' .number_format($fruits[$item['product']]['price']*$item['quantity'], 2) . '</span>';
                             echo '</li>';
                         }
                     }
                     ?>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Product name</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$12</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Second product</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$8</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Third item</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$5</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between bg-light">
-                    <div class="text-success">
-                        <h6 class="my-0">Promo code</h6>
-                        <small>EXAMPLECODE</small>
-                    </div>
-                    <span class="text-success">-$5</span>
-                    </li>
                     <li class="list-group-item d-flex justify-content-between">
                     <span>Total (USD)</span>
-                    <strong>$20 <?php echo ' real total: '.$total; ?></strong>
+                    <strong><?php echo $total; ?></strong>
                     </li>
                 </ul>
             </div>
--- assignmentW03-checkout.php
+++ assignmentW03-checkout.php
@@ -139,54 +139,26 @@ switch ($action) {
             <span class="badge badge-secondary badge-pill"> <?php echo itemCountInCart(); ?></span>
           </h4>
           <ul class="list-group mb-3">
-            <!-- make cart li items -->
-            <?php
-            foreach ($_SESSION['cart'] as $key => $val) {
-                if ($val['product'] === $item) {
-                    echo '<li class="list-group-item d-flex justify-content-between lh-condensed">';
-                    echo '<div>';
-                    echo '<h6 class="my-0">Product name</h6>';
-                }
-            }
-            ?>
-
-
-
-
-
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
+          <?php
+                    if ( isset($_SESSION['cart']) ) {
+                        $total = 0;
+                        foreach($_SESSION['cart'] as $item){
+                            $total += $fruits[$item['product']]['price']*$item['quantity'];
+                            echo '<li class="list-group-item d-flex justify-content-between lh-condensed">';
+                            echo '<di>';
+                            echo '<h6 class="my-0">'. $item['product'] . '</h6>';
+                            echo '<small class="text-muted">'. $fruits[$item['product']]['desc'] . '<br>Quantity:'.$item['quantity']. '</small>';
+                            echo '</di>';
+                            echo '<span class="text-muted">$' .number_format($fruits[$item['product']]['price']*$item['quantity'], 2) . '</span>';
+                            echo '</li>';
+                        }
+                    }
+                    ?>
+                    <li class="list-group-item d-flex justify-content-between">
+                    <span>Total (USD)</span>
+                    <strong><?php echo $total; ?></strong>
+                    </li>
+                </ul>
         </div>
         <div class="col-md-8 order-md-1">
           <h4 class="mb-3">Billing address</h4>
