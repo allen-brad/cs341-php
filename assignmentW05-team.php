cf841479cbfc7cdfaa26c0d0fae4e7be82976379
Brad R. Allen <bradrallen@gmail.com>
2020-05-17 00:24:18 -0600

    w05 team

---
+++ assignmentW05-team.php
@@ -0,0 +1,72 @@
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+function getScriptureById($scriptureId){
+    $sql = 'SELECT * FROM Scriptures WHERE id = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':id', $scriptureId, PDO::PARAM_INT);
+    $stmt->execute();
+    $scripture = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $scripture;
+  }
+
+  function getScripturesByBook($book){
+    $sql = 'SELECT * FROM Scriptures WHERE book = :book';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    $stmt->execute();
+    $scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $scriptures;
+   }
+
+   $sql = 'SELECT * FROM Scriptures';
+   $stmt = $db->prepare($sql);
+   $stmt->execute();
+   $allScriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   $stmt->closeCursor();
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Week 05 Team Assignment">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>CS341 W05 Team Assignment</title>
+</head>
+
+<body>
+<?php
+    print_r ($allScriptures).'<br>';
+    print_r (getScriptureById(1)).'<br>';
+    print_r (getScripturesByBook('Mosiah')).'<br>';
+?>
+</body>
+</html>
