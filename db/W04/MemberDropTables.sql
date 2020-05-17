174ec6798af650618305f2cddf77bd6bfcd4414b
Brad R. Allen <bradrallen@gmail.com>
2020-05-16 20:03:00 -0600

    adding DB files

---
+++ db/W04/MemberDropTables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS "public"."MemberTenure";
+DROP TABLE IF EXISTS "public"."MemberPhone";
+DROP TABLE IF EXISTS "public"."MemberEmergencyContact";
+DROP TABLE IF EXISTS "public"."MemberAddress";
+DROP TABLE IF EXISTS "public"."Member";
+DROP TABLE IF EXISTS "public"."MemberStatus";
