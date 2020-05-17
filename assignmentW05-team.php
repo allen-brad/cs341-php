0594924d26e92f0c8269c888162f0e9800a80c92
Brad R. Allen <bradrallen@gmail.com>
2020-05-17 00:30:33 -0600

    w05 team

--- assignmentW05-team.php
+++ assignmentW05-team.php
@@ -3,31 +3,34 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+function dbConnect(){
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
 }
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+
 
 
 function getScriptureById($scriptureId){
+    dbConnect();
     $sql = 'SELECT * FROM Scriptures WHERE id = :id';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':id', $scriptureId, PDO::PARAM_INT);
@@ -38,6 +41,7 @@ function getScriptureById($scriptureId){
   }
 
   function getScripturesByBook($book){
+    dbConnect();
     $sql = 'SELECT * FROM Scriptures WHERE book = :book';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':book', $book, PDO::PARAM_STR);
@@ -46,7 +50,8 @@ function getScriptureById($scriptureId){
     $stmt->closeCursor();
     return $scriptures;
    }
-
+   
+   dbConnect();
    $sql = 'SELECT * FROM Scriptures';
    $stmt = $db->prepare($sql);
    $stmt->execute();
