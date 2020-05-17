95081b6fbedf29e6debb9bd866205c1583abe9a6
Brad R. Allen <bradrallen@gmail.com>
2020-05-17 00:37:46 -0600

    w05 team

--- assignmentW05-team.php
+++ assignmentW05-team.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-function dbConnect(){
+
     try
     {
       $dbUrl = getenv('DATABASE_URL');
@@ -26,12 +26,32 @@ function dbConnect(){
       echo 'Error!: ' . $ex->getMessage();
       die();
     }
-}
 
 
 
 function getScriptureById($scriptureId){
-    dbConnect();
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
     $sql = 'SELECT * FROM Scriptures WHERE id = :id';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':id', $scriptureId, PDO::PARAM_INT);
@@ -42,7 +62,28 @@ function getScriptureById($scriptureId){
   }
 
   function getScripturesByBook($book){
-    dbConnect();
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
     $sql = 'SELECT * FROM Scriptures WHERE book = :book';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':book', $book, PDO::PARAM_STR);
@@ -51,13 +92,36 @@ function getScriptureById($scriptureId){
     $stmt->closeCursor();
     return $scriptures;
    }
-
-   dbConnect();
+function getAllScriptures(){
+   try
+   {
+     $dbUrl = getenv('DATABASE_URL');
+   
+     $dbOpts = parse_url($dbUrl);
+   
+     $dbHost = $dbOpts["host"];
+     $dbPort = $dbOpts["port"];
+     $dbUser = $dbOpts["user"];
+     $dbPassword = $dbOpts["pass"];
+     $dbName = ltrim($dbOpts["path"],'/');
+   
+     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+   
+     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     return $db;
+   }
+   catch (PDOException $ex)
+   {
+     echo 'Error!: ' . $ex->getMessage();
+     die();
+   }
    $sql = 'SELECT * FROM Scriptures';
    $stmt = $db->prepare($sql);
    $stmt->execute();
    $allScriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
    $stmt->closeCursor();
+   return $allScriptures;
+}
 
 ?>
 
