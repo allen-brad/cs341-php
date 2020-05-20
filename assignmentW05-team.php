0047916b46ea001011dbed68efb75d2f67f0d8d9
Brad R. Allen <bradrallen@gmail.com>
2020-05-19 20:14:18 -0600

    team meeting

--- assignmentW05-team.php
+++ assignmentW05-team.php
@@ -19,6 +19,7 @@ function dbConnect() {try
       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
     }
     catch (PDOException $ex)
     {
